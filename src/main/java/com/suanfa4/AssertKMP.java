27741ca7ae87a15c452006dc688c9889518f457e
Sailor-wu <2856346101@qq.com>
2020-03-06 16:05:21 +0800

    添加以文件的形式读入源字符串匹配 和 中文匹配

--- src/main/java/com/suanfa4/AssertKMP.java
+++ src/main/java/com/suanfa4/AssertKMP.java
@@ -1,15 +1,46 @@
 package com.suanfa4;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class AssertKMP {
-	public static void main(String[] args) {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws FileNotFoundException {
+		assertKmp();
+		
+		String  string = "大苏打撒asdsa少i大苏打撒打算安定aasasa";
+		for (int i = 0; i < string.length(); i++) {
+			System.out.println(string.charAt(i));
+		}
+	}
+
+	private static void assertKmp() throws FileNotFoundException {
 		// 查找的字符
-		String paString = "abcs";
-		// 源文本串
+		String paString = "中国的撒旦";
 		String searchStr = "abcdabcsabg";
+		File file = new File("src/main/java/com/suanfa4/source.txt");
+		FileInputStream fileInputStream = new FileInputStream(file);
+		try {
+			byte [] buf = new byte [fileInputStream.available()];
+			fileInputStream.read(buf);
+			fileInputStream.close();
+			searchStr = new String(buf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+		// 源文本串
 		KMP kmp = new KMP(paString);
 		// 返回找到的下标
 		int search = kmp.search(searchStr);
 		System.out.println(search);
-		System.out.println(searchStr.substring(search, search+paString.length()));
+		if (search >=0) {			
+			System.out.println(searchStr.substring(search, search+paString.length()));
+		}
 	}
 }
--- src/main/java/com/suanfa4/KMP.java
+++ src/main/java/com/suanfa4/KMP.java
@@ -2,32 +2,38 @@ package com.suanfa4;
 
 public class KMP {
 
-	private int [][] dp;
+	private int[][] dp;
 	private String part;
-	
-	
+
+	/**
+	 * 用 pat 表示模式串，长度为 M，txt 表示文本串，长度为 N。<br/>
+	 * KMP 算法是在 txt 中查找子串<br/>
+	 * pat，如果存在，返回这个子串的起始索引，否则返回 -1<br/>
+	 * 
+	 * @param part
+	 */
 	public KMP(String part) {
 		this.part = part;
 		int m = part.length();
-		
+
 		// dp[状态][字符] = 下个状态
-		dp = new int [m][256];
+		// 由于中文的 Ascii 码不确定。这里给了比较大（256*256）
+		dp = new int[m][256*256];
 		dp[0][part.charAt(0)] = 1;
-		
+
 		int x = 0;
-		
+
 		// 构建状态转移图
 		for (int i = 1; i < m; i++) {
 			for (int j = 0; j < 256; j++) {
 				dp[i][j] = dp[x][j];
 			}
-			dp[i][part.charAt(i)] = i +1;
+			dp[i][part.charAt(i)] = i + 1;
 			// 更新
 			x = dp[x][part.charAt(i)];
 		}
-		
 	}
-	
+
 	public int search(String text) {
 		int M = part.length();
 		int N = text.length();
@@ -35,11 +41,12 @@ public class KMP {
 		for (int i = 0; i < N; i++) {
 			// 计算下一个状态
 			j = dp[j][text.charAt(i)];
-			//到达终止态，返回结果
+			// 到达终止态，返回结果
 			if (j == M) {
 				return i - M + 1;
 			}
 		}
 		return -1;
 	}
+	
 }
