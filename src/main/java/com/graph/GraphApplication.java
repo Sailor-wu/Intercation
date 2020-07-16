01aeb7d3310dda03e2f6665eea5bad15604e36ea
Sailor-wu <2856346101@qq.com>
2020-07-16 15:26:17 +0800

    修改测试的位置

--- src/main/java/com/graph/GraphApplication.java
+++ src/main/java/com/graph/GraphApplication.java
@@ -1,13 +1,27 @@
 package com.graph;
+ 
 
-import java.util.Scanner;
+import com.graph.adjacency.AlGraph;
+import com.graph.matrix.AMGraph;
+import com.graph.matrix.BFSTraverse;
+import com.graph.matrix.DFSTraverse;
 
 public class GraphApplication {
 
 	public static void main(String[] args) {
-//		new AMGraph(3,2);
+		int vexNum = 3;// 顶点总数
+		int arcNum = 2;// 总边数
+		AlGraph graph = new AlGraph(vexNum, arcNum);
+		System.out.println("深度遍历得到：");
+		graph.DFSTraverse();
+		System.out.println("广度遍历得到：");
+		graph.BFSTraverse();
 		
-		Scanner sc = new Scanner(System.in);
+	}
+	
+	public static void main1(String[] args) {
+//		new AMGraph(3,2);
+		 
 		int vexNum = 3;// 顶点总数
 		int arcNum = 2;// 总边数
 		AMGraph amGraph = new AMGraph(vexNum,arcNum);
