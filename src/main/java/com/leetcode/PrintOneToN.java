3e8b93278a8e578299c2882dff6072352cf4084e
Sailor-wu <2856346101@qq.com>
2020-07-23 13:09:55 +0800

    打印0到n

---
+++ src/main/java/com/leetcode/PrintOneToN.java
@@ -0,0 +1,48 @@
+package com.leetcode;
+
+public class PrintOneToN {
+
+	public static void main(String[] args) {
+		System.out.println(new PrintOneToN().printNumbers(3));
+	}
+	StringBuilder res;
+    int count = 0, n,nine = 0,start;
+    char[] num, loop = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    public String printNumbers(int n) {
+        this.n = n;
+        res = new StringBuilder(); // 数字字符串集
+        num = new char[n]; // 定义长度为 n 的字符列表
+        start = n - 1;
+        dfs(0); // 开启全排列递归
+        res.deleteCharAt(res.length() - 1); // 删除最后多余的逗号
+        return res.toString(); // 转化为字符串并返回
+    }
+    /**
+     * 带0
+     * @param x
+     */
+    void dfs_0(int x) {
+        if(x == n) { // 终止条件：已固定完所有位
+            res.append(String.valueOf(num) + ","); // 拼接 num 并添加至 res 尾部，使用逗号隔开
+            return;
+        }
+        for(char i : loop) { // 遍历 ‘0‘ - ’9‘
+            num[x] = i; // 固定第 x 位为 i
+            dfs(x + 1); // 开启固定第 x + 1 位
+        }
+    } 
+    void dfs(int x) {
+        if(x == n) {
+            String s = String.valueOf(num).substring(start);
+            if(!s.equals("0")) res.append(s + ",");
+            if(n - start == nine) start--;
+            return;
+        }
+        for(char i : loop) {
+            if(i == '9') nine++;
+            num[x] = i;
+            dfs(x + 1);
+        }
+        nine--;
+    } 
+}
